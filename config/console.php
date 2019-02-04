2bc01da0ac4e1c973367e0c93000fcf5122706b4
Ivan <devskyfly@gmail.com>
2019-02-04 16:16:50 +0300

    Change configs.

--- config/console.php
+++ config/console.php
@@ -14,27 +14,9 @@ $config = [
         '@tests' => '@app/tests',
     ],
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
     ],
     'params' => $params,
-    /*
-     'controllerMap' => [
-     'fixture' => [ // Fixture generation command line.
-     'class' => 'yii\faker\FixtureController',
-     ],
-     ],
-     */
+    
 ];
 
 if (YII_ENV_DEV) {
