a911cc4f0a6094f9cb49a50d18b5540898fb68a6
Ivan <devskyfly@gmail.com>
2019-04-09 09:59:25 +0300

    Add ErrorAgentsList and ErrorSettlementList widgets.

--- views/default/index.php
+++ views/default/index.php
@@ -3,6 +3,9 @@
 /* $list []*/
 /* $title string */
 use devskyfly\yiiModuleAdminPanel\widgets\common\NavigationMenu;
+use devskyfly\yiiModuleIitPartners\widgets\ErrorAgentsList;
+use yii\base\Widget;
+use devskyfly\yiiModuleIitPartners\widgets\ErrorSettlementsList;
 
 ?>
 <?
@@ -11,4 +14,8 @@ $this->title=$title;
 
 <div class="col-xs-3">
 <?=NavigationMenu::widget(['list'=>$list])?>
+</div>
+<div class="col-xs-9">
+<?=ErrorAgentsList::widget()?>
+<?=ErrorSettlementsList::widget()?>
 </div>
---
+++ widgets/ErrorAgentsList.php
@@ -0,0 +1,41 @@
+<?php
+namespace devskyfly\yiiModuleIitPartners\widgets;
+
+use devskyfly\php56\types\Vrbl;
+use devskyfly\yiiModuleIitPartners\models\Agent;
+use yii\base\Widget;
+use devskyfly\yiiModuleIitPartners\models\Settlement;
+
+class ErrorAgentsList extends Widget
+{
+    public $data;
+    
+    public function init()
+    {
+        parent::init();
+        $agents=Agent::find()->each();
+        foreach ($agents as $agent){
+            $result=false;
+            
+            if(Vrbl::isNull($agent['_settlement__id'])
+                ||Vrbl::isEmpty($agent['_settlement__id'])){
+                    $result=true;
+            }
+            
+            $settlement=Settlement::find()->where(['id'=>$agent->_settlement__id])->one();
+            if(!$settlement){
+                $result=true;
+            }
+            
+            if($result){
+                $this->data[]=$agent;
+            }
+        }
+    }
+    
+    public function run()
+    {
+        $data=$this->data;
+        return $this->render('error-agents-list',compact("data"));
+    }
+}
